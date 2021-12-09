a6e5db2704c909554fe09faf6533708752ac13ef
Jonatas Martins <jontas.martins@seplag.ce.gov.br>
2021-12-09 17:29:13 -0300

    Starting create templates

--- index.js
+++ index.js
@@ -1,13 +1,23 @@
-const express = require('express')
-const Product = require('./routers/product')
-const app = express()
-const PORT = 8080
+const express = require('express');
+const Product = require('./controllers/product');
+const app = express();
+const PORT = 3000;
 
-app.use(express.json())
-app.use(express.urlencoded({ extended: true }))
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use('/public', express.static(__dirname + '/public'));
 
-app.use('/produtos', Product)
+app.use((req, resp, next) => {
+    req.path.includes("/user") && !req.headers.authorization ? resp.redirect("/") : next();
+});
+
+app.use('/produtos', Product);
+
+app.get('/:template?(\.html)?', (req, resp) => {
+    const { template } = req.params;
+    resp.sendFile(__dirname + `/templates/${template || "index"}.html`);
+});
 
 app.listen(PORT, function() {
     console.log('listening on *:'+PORT)
-})
+});
